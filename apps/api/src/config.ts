fd4ca300cb8ef162cc7aa67bc99e0638f66ef93a
irfanhidayatullah <irfanhidayatullah.jobs@gmail.com>
2024-09-13 14:43:41 +0700

    chore: first setup

--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -11,3 +11,10 @@ config({ path: resolve(__dirname, `../${envFile}`) });
 
 export const PORT = process.env.PORT || 8000;
 export const DATABASE_URL = process.env.DATABASE_URL || '';
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const BASE_URL_FE = process.env.BASE_URL_FE;
+export const GMAIL_EMAIL = process.env.GMAIL_EMAIL;
+export const GMAIL_APP_PASSWORD = process.env.GMAIL_APP_PASSWORD;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
