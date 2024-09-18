756445789f96bb758960f261a1a5ef17fdff649a
irfanhidayatullah <167808141+irfanhidayatullah@users.noreply.github.com>
2024-09-18 17:20:30 +0700

    feat: resetPassword (#8)

--- apps/api/src/routers/auth.router.ts
+++ apps/api/src/routers/auth.router.ts
@@ -1,7 +1,5 @@
-// import { SampleController } from '@/controllers/sample.controller';
 import { AuthController } from '@/controllers/auth.controller';
 import { verifyToken } from '@/middlewares/verifyToken';
-// import { registerValidator } from '@/middlewares/registerValidator';
 import { Router } from 'express';
 
 export class AuthRouter {
@@ -16,18 +14,22 @@ export class AuthRouter {
 
   private initializeRoutes(): void {
     this.router.get('/:id', verifyToken, this.authController.getUserController);
-    this.router.post(
-      '/register',
-      // registerValidator,
-      this.authController.registerController,
-    );
+    this.router.post('/register', this.authController.registerController);
     this.router.patch(
       '/verification',
       verifyToken,
-      // verificationValidator,
       this.authController.verifyController,
     );
     this.router.post('/login', this.authController.login);
+    this.router.post(
+      '/forgot-password',
+      this.authController.forgotPasswordController,
+    );
+    this.router.patch(
+      '/reset-password',
+      verifyToken,
+      this.authController.resetPasswordController,
+    );
     // this.router.post('/google', this.authController.GoogleController);
   }
 
